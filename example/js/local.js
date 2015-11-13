975e8a1dcf746e1eaa638a86aa584c97bd23e07d
chemzqm <chemzqm@gmail.com>
2015-11-14 04:02:08 +0800

    add local example

--- example/js/local.js
+++ example/js/local.js
@@ -3,25 +3,52 @@ require('pull-to-refresh/ptr.css')
 require('more-mobile/more.css')
 var List = require('../..')
 var ALL_DATA = require('../data.json')
-var USERS = ALL_DATA.slice(0, 30)
-var PREPEND_USERS = ALL_DATA.slice(30, 60)
+var event = require('event')
+var tap = require('tap-event')
 
 var scrollable = document.querySelector('.scrollable')
+var ul = scrollable.querySelector('ul')
 
 function setCount() {
   var el = document.getElementById('count')
-  var ul = scrollable.querySelector('ul')
-  ul.style.minHeight = (40 * USERS.length) + 'px'
   el.textContent = ul.querySelectorAll('li').length
 }
-var template = '<li data-id="{id}">{age} {text | truncate}</li>'
+var template = '<li data-id="{id}">{age} <span class="text">{text}</span></li>'
 
 var list = new List(template, scrollable, {
-  filters: {
-    truncate: function (str) {
-      return str.substr(0, 40) + '...'
-    }
-  },
   moreCount: 8,
-  limit: 15
+  limit: 20
 })
+list.local()
+
+
+list.on('change', function () {
+  setCount()
+})
+
+list.setData(ALL_DATA)
+
+var $ = function (id) {
+  return document.getElementById(id)
+}
+event.bind($('content'), 'input', function (e) {
+  var v = e.target.value
+  list.filter('text', v)
+})
+
+event.bind($('sort'), 'change', function (e) {
+  var v = e.target.value
+  if (v) {
+    list.sort(v, $('order').value)
+  }
+})
+
+event.bind($('order'), 'change', function (e) {
+  var v = e.target.value
+  list.sort($('sort').value, v)
+})
+
+list.bind('touchstart', '.text', tap(function (e, model) {
+  model.age = model.age + 1
+}))
+
